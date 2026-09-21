1b0c306a5a8ae547866673f3e93e3369d3051c04
Ayush kumar <ayushkumar10963@gmail.com>
2026-09-21 17:46:25 +0530

    created js file

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const descInput = document.getElementById('descInput');
 const amountInput = document.getElementById('amountInput');
 const categorySelect = document.getElementById('categorySelect');
 const addBtn = document.getElementById('addBtn');
+const expensesList = document.getElementById('expensesList');
 
 // Add transaction when button is clicked
 addBtn.addEventListener('click', function() {
@@ -26,16 +27,13 @@ addBtn.addEventListener('click', function() {
         return;
     }
 
-    // Convert amount from string to number
     const amount = Number(amountInput.value);
 
-    // Validate amount
     if (amount <= 0) {
         console.log("Enter a valid amount");
         return;
     }
 
-    // Create transaction object
     const transaction = {
         description: descInput.value,
         amount: amount,
@@ -43,7 +41,24 @@ addBtn.addEventListener('click', function() {
         type: selectedType.value
     };
 
-    // Store transaction in the array
     transactions.push(transaction);
 
+     // Create transaction card
+    const div = document.createElement('div');
+    div.classList.add('expense-item');
+
+    // Create information section
+    const infoDiv = document.createElement('div');
+    infoDiv.classList.add('expense-info');
+
+    // Add transaction description
+    infoDiv.textContent = transaction.description;
+
+    // Add information section to transaction card
+    div.appendChild(infoDiv);
+
+    // Add transaction card to the expenses list
+    expensesList.appendChild(div);
+
+    
 });
